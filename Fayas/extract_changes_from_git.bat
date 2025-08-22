b4a1be32bb34f9cf55eb038a2e1a4ae067c308b8
Sharpentine <fayyas.n.m.f@gmail.com>
2025-08-22 13:28:51 +0530

    Made Changes

---
+++ Fayas/extract_changes_from_git.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "C:\Users\loq\Documents\node centrality\Node-Centrality\Fayas"
+echo Pulling latest changes...
+git pull origin main
+pause
---
+++ Fayas/make_changes_to_git.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\loq\Documents\node centrality\Node-Centrality\Fayas"
+git add .
+git commit -m "Made Changes"
+git push origin main
+pause
