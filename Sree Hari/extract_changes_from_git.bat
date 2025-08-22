bdebc308a47e5ebde3dda9ee7011fdf62f93017f
Sreehari <sreeharimanakyalath40@gmail.com>
2025-08-22 13:32:08 +0530

    Made Changes

---
+++ Sree Hari/extract_changes_from_git.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "D:\#DIGITAL UNIVERSITY\Sem 3\Computational non linear dynamics\Node-Centrality"
+echo Pulling latest changes...
+git pull origin main
+pause
---
+++ Sree Hari/make_changes_to_git.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "D:\#DIGITAL UNIVERSITY\Sem 3\Computational non linear dynamics\Node-Centrality"
+git add .
+git commit -m "Made Changes"
+git push origin main
+pause
